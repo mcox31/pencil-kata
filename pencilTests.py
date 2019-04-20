7d000b70f6259dd43c1a6b39be12c6d38bbfb1dd
Michael <mcox31@emich.edu>
2019-04-20 08:57:11 -0400

    Added setup and teardown functions for readability and DRY

--- pencilTests.py
+++ pencilTests.py
@@ -2,19 +2,27 @@ import unittest
 from pencil import Pencil
 
 class PencilTester(unittest.TestCase):
+    
+    def setUp(self):
+        """Runs once per test to define a fresh object."""
+        self.my_pencil = Pencil(10)
+    
+    def tearDown(self):
+        """Runs once per test to delete object"""
+        del(self.my_pencil)
+
+
     def test_if_pencil_writes_a_word_it_is_outputted(self):
-        my_pencil = Pencil(10)
-        self.assertEqual(my_pencil.write("hello"), "hello")
+        self.assertEqual(self.my_pencil.write("hello"), "hello")
 
     def test_if_pencil_writes_another_word_it_is_appended_to_the_sheet(self):
-        my_pencil = Pencil(10)
-        my_pencil.write("hello")
-        self.assertEqual(my_pencil.write(" world!"), "hello world!")
+        self.my_pencil.write("hello")
+        self.assertEqual(self.my_pencil.write(" world!"), "hello world!")
 
     def test_if_pencil_writes_too_many_characters_its_durability_goes_to_zero(self):
-        my_pencil = Pencil(4)
-        my_pencil.write("hello")
-        self.assertEqual(my_pencil.check_durability(), 0)
+        self.my_pencil.set_durability(4)
+        self.my_pencil.write("hello")
+        self.assertEqual(self.my_pencil.check_durability(), 0)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)

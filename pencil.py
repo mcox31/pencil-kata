7b9043fa4a0f7436041c24a902fe69a2225267fd
Michael <mcox31@emich.edu>
2019-04-20 08:46:49 -0400

    Third test and some cleanup.

--- pencil.py
+++ pencil.py
@@ -1,8 +1,29 @@
 class Pencil():
 
-    def __init__(self):
+    def __init__(self, durability):
+        """Initializer. Now sets durability on object creation."""
         self.sheet_of_paper = ""
+        self.durability = durability
 
     def write(self, words):
+        """My pencil writes on a sheet of paper. Writing more words means more words on the page."""
+        self.calculate_durability(words)
         self.sheet_of_paper = self.sheet_of_paper + words
         return self.sheet_of_paper
+
+    def set_durability(self, durability):
+        """Change durability of pencil tip."""
+        self.durability = durability
+
+    def check_durability(self):
+        """Explicit call to check durability of pencil tip."""
+        return self.durability
+
+    def calculate_durability(self, words):
+        """Calculates durability from written words. If durability is greater, returns durability after words. Otherwise returns zero durability"""
+        if self.check_durability() > len(words):
+            self.set_durability(self.durability - len(words))
+        else:
+            self.set_durability(0)
+
+        
--- pencilTests.py
+++ pencilTests.py
@@ -3,13 +3,18 @@ from pencil import Pencil
 
 class PencilTester(unittest.TestCase):
     def test_if_pencil_writes_a_word_it_is_outputted(self):
-        my_pencil = Pencil()
+        my_pencil = Pencil(10)
         self.assertEqual(my_pencil.write("hello"), "hello")
 
     def test_if_pencil_writes_another_word_it_is_appended_to_the_sheet(self):
-        my_pencil = Pencil()
+        my_pencil = Pencil(10)
         my_pencil.write("hello")
         self.assertEqual(my_pencil.write(" world!"), "hello world!")
 
+    def test_if_pencil_writes_too_many_characters_its_durability_goes_to_zero(self):
+        my_pencil = Pencil(4)
+        my_pencil.write("hello")
+        self.assertEqual(my_pencil.check_durability(), 0)
+
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)

8dfae9cc4f4e3378f1b6f8c5c294c64f8f9d27a9
Michael <mcox31@emich.edu>
2019-04-20 07:53:42 -0400

    passed the first test.

---
+++ pencil.py
@@ -0,0 +1,3 @@
+class Pencil():
+    def write(self, words):
+        return words
--- pencilTests.py
+++ pencilTests.py
@@ -1,2 +1,10 @@
 import unittest
-import pencil
+from pencil import Pencil
+
+class PencilTester(unittest.TestCase):
+    def test_if_pencil_writes_a_word_it_is_outputted(self):
+        my_pencil = Pencil()
+        self.assertEqual(my_pencil.write("hello"), "hello")
+
+if __name__ == "__main__":
+    unittest.main()

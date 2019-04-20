0c5911723ac86a96284fe10f8cffd26f34eefdba
Michael <mcox31@emich.edu>
2019-04-20 07:59:29 -0400

    wrote and passed second test.

--- pencil.py
+++ pencil.py
@@ -1,3 +1,8 @@
 class Pencil():
+
+    def __init__(self):
+        self.sheet_of_paper = ""
+
     def write(self, words):
-        return words
+        self.sheet_of_paper = self.sheet_of_paper + words
+        return self.sheet_of_paper
--- pencilTests.py
+++ pencilTests.py
@@ -6,5 +6,10 @@ class PencilTester(unittest.TestCase):
         my_pencil = Pencil()
         self.assertEqual(my_pencil.write("hello"), "hello")
 
+    def test_if_pencil_writes_another_word_it_is_appended_to_the_sheet(self):
+        my_pencil = Pencil()
+        my_pencil.write("hello")
+        self.assertEqual(my_pencil.write(" world!"), "hello world!")
+
 if __name__ == "__main__":
     unittest.main()
